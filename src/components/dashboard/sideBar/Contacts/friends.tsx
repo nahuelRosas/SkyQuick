56e286f134379bea66339b3ef9d66f2ce8a36cda
nahuelRosas <nahuel.rosas21@gmail.com>
2023-01-25 19:19:25 -0300

    V0.1.6 update: add responsive layout -- fix error in friends layout

--- src/components/dashboard/sideBar/Contacts/friends.tsx
+++ src/components/dashboard/sideBar/Contacts/friends.tsx
@@ -1,15 +1,23 @@
-import { Flex } from '@chakra-ui/react';
-import { DocumentData } from 'firebase/firestore';
-import React from 'react';
+import { Flex } from "@chakra-ui/react";
+import { DocumentData } from "firebase/firestore";
+import React from "react";
 
-import useRecoveryData from '../../../../hooks/useRecoveryData';
-import ContactOnFriends from './ContactOnFriends';
+import useRecoveryData from "../../../../hooks/useRecoveryData";
+import ContactOnFriends from "./ContactOnFriends";
 
 const Friends = () => {
   const { recoverData } = useRecoveryData();
   const Friends = recoverData("friends") as DocumentData[];
   return (
-    <Flex w={"100%"}>
+    <Flex
+      w={"100%"}
+      h={"auto"}
+      flexDir="column"
+      alignItems="center"
+      justifyContent="center"
+      gap={4}
+      p={5}
+      overflow={"auto"}>
       {Friends?.map(
         (Friends: DocumentData, key: React.Key | null | undefined) => {
           return <ContactOnFriends key={key} hit={Friends} />;
